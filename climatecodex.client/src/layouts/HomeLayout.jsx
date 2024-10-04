43ab4c68af0508573b13c7af6558b21f30f09eb0
EHAB3x <ehabmah6969@gmail.com>
2024-10-04 07:31:26 +0200

    finish moving text component

--- climatecodex.client/src/layouts/HomeLayout.jsx
+++ climatecodex.client/src/layouts/HomeLayout.jsx
@@ -1,9 +1,11 @@
 import Hero from '../components/climaflow/Home/hero/Hero'
+import MovingText from '../components/climaflow/Home/movingText/MovingText'
 
 const HomeLayout = () => {
   return (
     <>
         <Hero />
+        <MovingText />
     </>
   )
 }
