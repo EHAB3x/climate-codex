56a084932479a17066daa99c4f593b3cfd79b1f4
EHAB3x <ehabmah6969@gmail.com>
2024-10-04 23:42:27 +0200

    finish methane at home component

---
+++ climatecodex.client/src/components/climaflow/Home/methane/Methane.jsx
@@ -0,0 +1,30 @@
+import './Methane.scss'
+import { Link } from "react-router-dom";
+import Methane_img from "../../../../assets/images/methane/stats.png"
+const Methane = () => {
+  return (
+    <div className="methane py-16">
+      <div className="methane__container container mx-auto flex sm:flex-row flex-col items-center justify-center gap-8">
+        <div className="gas__statistics flex flex-col items-center gap-6">
+          <div className="gas__card flex sm:flex-row flex-col justify-between gap-8 text-center">
+            <img src={Methane_img} alt="Methane" />
+          </div>
+        </div>
+
+        <div className="methane__content relative flex flex-col gap-7">
+          <h2>
+            Methane and Its Impact
+          </h2>
+
+          <p>
+            Methane is a potent greenhouse gas that contributes heavily to climate change and can degrade air quality. Learn how methane levels affect your environment and find out ways to track and reduce its impact.
+          </p>
+
+          <Link to={"/co2"} className="secondary__btn">Track Methane Impact</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Methane
--- climatecodex.client/src/layouts/HomeLayout.jsx
+++ climatecodex.client/src/layouts/HomeLayout.jsx
@@ -3,6 +3,7 @@ import Insights from '../components/climaflow/Home/insights/Insights'
 import MovingText from '../components/climaflow/Home/movingText/MovingText'
 import Co2 from '../components/climaflow/Home/co2/Co2'
 import Nav from '../components/common/nav/nav'
+import Methane from '../components/climaflow/Home/methane/Methane'
 
 const HomeLayout = () => {
   return (
@@ -12,6 +13,7 @@ const HomeLayout = () => {
         <MovingText />
         <Insights />
         <Co2 />
+        <Methane />
     </>
   )
 }
